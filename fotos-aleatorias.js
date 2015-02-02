6b51a14f7408bc3dcfc8613389ed63c38acb82c1
Jesus Alberto Saiz <ja.saizc@alumnos.urjc.es>
2015-02-02 14:39:13 +0100

    primera version del programa

---
+++ fotos-aleatorias.js
@@ -0,0 +1,35 @@
+fotos = ['http://c4.staticflickr.com/8/7369/15799001574_1315f607d1_n.jpg', 
+         'http://c1.staticflickr.com/9/8625/15794208943_30743c6017_n.jpg' , 
+         'http://c1.staticflickr.com/9/8654/16233056440_344d792f0d_n.jpg',
+         'http://c2.staticflickr.com/8/7356/15795113883_e5f51a7a1b_n.jpg']
+         
+function changer (id, newValue) {
+  var element = document.getElementById(id);
+  element.style.visibility = 'visible';
+  element.setAttribute('src', newValue);
+}
+
+function CambiaFotos(){
+    imagenrandom = imagenAleatoria()
+
+    changer('imagen', imagenrandom)
+    
+}
+function CambiaFotosEnHTML(){
+    imagenrandom = imagenAleatoria()
+
+    changer('imagen', imagenrandom)
+    
+}
+
+function aleatorio(inferior,superior){
+    var numPosibilidades = superior - inferior
+    var aleat = Math.random() * numPosibilidades
+    aleat = Math.round(aleat)
+    return parseInt(inferior) + aleat
+} 
+
+function imagenAleatoria(){
+    primervalor = aleatorio(0,(fotos.length - 1));
+    return fotos[primervalor];
+}
